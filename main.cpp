ec6f3910bb8bff13192ab06930b4502db5f4caba
neetmehta <neetmehta19@gmail.com>
2022-05-28 15:38:30 -0400

    fixed_bug

--- main.cpp
+++ main.cpp
@@ -9,27 +9,30 @@
 
 struct Point;
 struct Point1Compare;
-std::vector<Point> get_neighbors(std::vector<std::string>& map, Point point, Point start, Point goal, std::vector<std::vector<bool>>& visited, std::map<std::vector<int>,std::vector<int>>& mp);
+std::vector<Point> get_neighbors(std::vector<std::string>& map, Point point, Point start, Point goal, std::vector<std::vector<bool>>& visited, std::map<std::vector<int>, std::vector<int>>& mp);
 void visualize(std::vector<std::string> map);
 void claculate_heuristic(const Point& start, const Point& goal, Point& point);
 void backtrack(std::vector<std::string>& map, Point goal, std::map<std::vector<int>, std::vector<int>>& mp);
-std::vector<std::string> map = {  "                  ",
-								  "                  ",
-								  "     ####         ",
-								  "                  ",
-								  "S         ####    ",
-								  " ####    #####    ",
-								  "                  ",
-								  "        #####     ",
-								  "                  ",
-								  "                 G", };
+std::vector<std::string> map = {  "####################",
+								  "#                  #",
+								  "#                  #",
+								  "#     ####         #",
+								  "#          #       #",
+								  "# S        ####    #",
+								  "# #############    #",
+								  "#                  #",
+								  "#  #     #####     #",
+								  "#   #            G #",
+								  "#    #             #",
+								  "####################", };
 
 struct Point
 {
 public:
 	int x;
 	int y;
-	int heuristic=INT_MAX;
+	int home_step = 0;
+	int heuristic = INT_MAX;
 	Point(int x_cord, int y_cord) : x(x_cord), y(y_cord) {};
 	Point() :x(0), y(0) {};
 };
@@ -62,11 +65,10 @@ void visualize(std::vector<std::string> map)
 void astar(std::vector<std::string>& map, Point& start, Point& goal)
 {
 	std::map<std::vector<int>, std::vector<int>> mp;
-
-	mp[std::vector<int>{start.x, start.y}] = std::vector<int>{-1,-1};
+	mp[std::vector<int>{start.x, start.y}] = std::vector<int>{ -1,-1 };
 	std::priority_queue<Point, std::vector<Point>, Compare> pq;
 	pq.push(start);
-	Point point=start;
+	Point point = start;
 	std::vector<Point> neighbors;
 	std::vector<std::vector<bool>> visited(map.size(), std::vector<bool>(map[0].size(), false));
 	while (!pq.empty())
@@ -90,11 +92,11 @@ void astar(std::vector<std::string>& map, Point& start, Point& goal)
 
 }
 
-void backtrack(std::vector<std::string>& map, Point goal, std::map<std::vector<int>,std::vector<int>>& mp)
+void backtrack(std::vector<std::string>& map, Point goal, std::map<std::vector<int>, std::vector<int>>& mp)
 {
 	int x = goal.x, y = goal.y;
 	int a, b;
-	while (x != -1 && y!=-1)
+	while (x != -1 && y != -1)
 	{
 		map[x][y] = '.';
 		a = mp[std::vector<int>{x, y}][0];
@@ -104,17 +106,19 @@ void backtrack(std::vector<std::string>& map, Point goal, std::map<std::vector<i
 	}
 }
 
-std::vector<Point> get_neighbors(std::vector<std::string>& map, Point point, Point start, Point goal, std::vector<std::vector<bool>>& visited, std::map<std::vector<int>,std::vector<int>>& mp)
+std::vector<Point> get_neighbors(std::vector<std::string>& map, Point point, Point start, Point goal, std::vector<std::vector<bool>>& visited, std::map<std::vector<int>, std::vector<int>>& mp)
 {
 	int x = point.x, y = point.y;
 	std::vector<std::vector<int>> nbs{ {1,0},{0,1},{-1,0},{0,-1} };
 	std::vector<Point> neighbors;
-	for (std::vector<int> nb:nbs)
+	for (std::vector<int> nb : nbs)
 	{
-		
-		if (x - nb[0] >= 0 && x - nb[0] < map.size() && y - nb[1]>=0 && y - nb[1] < map[0].size() && !visited[x-nb[0]][y-nb[1]])
+
+		if (x - nb[0] >= 0 && x - nb[0] < map.size() && y - nb[1] >= 0 && y - nb[1] < map[0].size() && !visited[x - nb[0]][y - nb[1]])
 		{
+			if (map[x - nb[0]][y - nb[1]] == '#') continue;
 			Point neighbor{ x - nb[0] , y - nb[1] };
+			neighbor.home_step = point.home_step + 1;
 			claculate_heuristic(start, goal, neighbor);
 			mp[std::vector<int>{neighbor.x, neighbor.y}] = std::vector<int>{ point.x,point.y };
 			neighbors.push_back(neighbor);
@@ -125,38 +129,37 @@ std::vector<Point> get_neighbors(std::vector<std::string>& map, Point point, Poi
 
 void claculate_heuristic(const Point& start, const Point& goal, Point& point)
 {
-	point.heuristic = abs(point.x - start.x) + abs(point.y - start.y) + abs(point.x - goal.x) + abs(point.y - goal.y);
+	point.heuristic = point.home_step + abs(point.x - goal.x) + abs(point.y - goal.y);
 }
 
 void find_s_g(Point& start, Point& goal, std::vector<std::string>& map)
 {
-    for(int i=0;i<map.size();i++)
-    {
-        for(int j=0;j<map[0].size();j++)
-        {
-            if(map[i][j]=='S')
-            {
-                start.x = i;
-                start.y = j;
-            }
+	for (int i = 0; i < map.size(); i++)
+	{
+		for (int j = 0; j < map[0].size(); j++)
+		{
+			if (map[i][j] == 'S')
+			{
+				start.x = i;
+				start.y = j;
+			}
 
-            if(map[i][j]=='G')
-            {
-                goal.x = i;
-                goal.y = j;
-            }
-        }
-    }
+			if (map[i][j] == 'G')
+			{
+				goal.x = i;
+				goal.y = j;
+			}
+		}
+	}
 }
 
 int main()
 {
 	Point start{ 0,0 };
 	Point goal{ 9,17 };
-    find_s_g(start, goal, map);
+	find_s_g(start, goal, map);
 	visualize(map);
 	astar(map, start, goal);
-	//backtrack(map, goal);
 	visualize(map);
 	return 0;
 }
